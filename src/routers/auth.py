60084a52ac8aec9549363fb2d5798d768283ad60
ramazanix <ramazanui2002@gmail.com>
2024-06-12 01:06:59 +0300

    Set cookies in response

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -67,5 +67,6 @@ async def auth_callback(request: Request):
     access_token = create_access_token(user_info)
 
     # Возврат JWT токена в качестве ответа
-    response = RedirectResponse(url=f'{settings.FRONTEND_URL}/?access_token={access_token}')
+    response = RedirectResponse(url=f'{settings.FRONTEND_URL}')
+    response.set_cookie('accessToken', access_token)
     return response
