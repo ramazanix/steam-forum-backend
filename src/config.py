aca87062aa973a94740c54e4badb09dd255f1d35
ramazanix <ramazanui2002@gmail.com>
2024-06-11 19:18:02 +0300

    Added dotenv fields

--- src/config.py
+++ src/config.py
@@ -2,6 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    STEAM_API_KEY: str
+    BACKEND_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_EXPIRES: int
+
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
