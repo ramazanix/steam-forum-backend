3b1e72169bc74f376d505ea7e5722a17053deb0e
ramazanix <ramazanui2002@gmail.com>
2024-06-11 19:25:22 +0300

    Fixes

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 import requests
+from src.config import settings
 
 
 def get_user_info(user_id):
     """
     Функция для получения информации о пользователе с помощью Steam API.
     """
-    url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={user_id}"
+    url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={settings.STEAM_API_KEY}&steamids={user_id}"
     response = requests.get(url)
 
     return response.json()['response']['players'][0]
