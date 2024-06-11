e982d367dd265f0e5333a2484eb61a7bf6b79939
ramazanix <ramazanui2002@gmail.com>
2024-06-11 18:16:55 +0300

    Refactored code

--- rest/routers/main.py
+++ rest/routers/main.py
@@ -1,54 +1,81 @@
-from fastapi import FastAPI, Request
+import os
+import requests
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import RedirectResponse
-from authlib.integrations.starlette_client import OAuth, OAuthError
-from starlette.middleware.sessions import SessionMiddleware
-from fastapi.middleware.cors import CORSMiddleware
+from urllib import parse
+
+from rest.security import create_access_token
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="C15A866FCD2B75D053EF27FD1B6BA6C")
-origins = [
-    "*"
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-oauth = OAuth()
-oauth.register(
-    name='steam',
-    client_id='C15A866FCD2B75D053EF27FD1B6BA6C6',
-    authorize_url='https://steamcommunity.com/oauth/login',
-    response_type='code',
-)
+# Конфигурация Steam OpenID Connect
+STEAM_OPENID_URL = 'https://steamcommunity.com/openid/login'
+STEAM_API_KEY = os.getenv('STEAM_API_KEY')
+REDIRECT_URI = f'{os.getenv("BACKEND_URL")}/auth/callback'
 
 
-@app.middleware('http')
-async def some_middleware(request: Request, call_next):
-    headers = dict(request.scope['headers'])
-    headers[b'Access-Control-Allow-Origin'] = b'*'
-    request.scope['headers'] = [(k, v) for k, v in headers.items()]
+@app.get("/auth/login")
+async def login():
+    """
+    Обработчик для перенаправления пользователя на страницу аутентификации Steam.
+    """
+    params = {
+        'openid.ns': 'http://specs.openid.net/auth/2.0',
+        'openid.mode': 'checkid_setup',
+        'openid.return_to': REDIRECT_URI,
+        'openid.realm': REDIRECT_URI,
+        'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
+        'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select',
+    }
+    return RedirectResponse(url=f'{STEAM_OPENID_URL}?{parse.urlencode(params)}')
 
-    return await call_next(request)
+
+@app.get('/auth/callback')
+async def auth_callback(request: Request):
+    """
+    Обработчик для обработки ответа от Steam после аутентификации.
+    """
+    params = request.query_params
+
+    if 'openid.mode' not in params or params['openid.mode'] != 'id_res':
+        raise HTTPException(status_code=400, detail='Invalid OpenID response')
+
+    # Проверка подлинности ответа от Steam
+    response = requests.get(
+        STEAM_OPENID_URL,
+        params={
+            'openid.assoc_handle': params['openid.assoc_handle'],
+            'openid.signed': params['openid.signed'],
+            'openid.sig': params['openid.sig'],
+            'openid.ns': 'http://specs.openid.net/auth/2.0',
+            'openid.mode': 'check_authentication',
+            'openid.op_endpoint': params['openid.op_endpoint'],
+            'openid.claimed_id': params['openid.claimed_id'],
+            'openid.identity': params['openid.identity'],
+            'openid.return_to': params['openid.return_to'],
+            'openid.response_nonce': params['openid.response_nonce'],
+        },
+    )
+
+    if 'is_valid:true' not in response.text:
+        raise HTTPException(status_code=401, detail='Invalid OpenID response')
+
+    # Извлечение информации о пользователе
+    user_id = params['openid.claimed_id'].split('/')[-1]
+    user_info = get_user_info(user_id)
+
+    # Генерация JWT токена
+    access_token = create_access_token(user_info)
+
+    # Возврат JWT токена в качестве ответа
+    return {'access_token': access_token}
 
 
-@app.get('/login/steam')
-async def login_steam(request: Request):
-    print(request.headers)
-    redirect_uri = request.url_for('authorize_steam')
-    return await oauth.steam.authorize_redirect(request, redirect_uri)
+def get_user_info(user_id):
+    """
+    Функция для получения информации о пользователе с помощью Steam API.
+    """
+    url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={user_id}"
+    response = requests.get(url)
 
-
-@app.get('/authorize/steam')
-async def authorize_steam(request: Request):
-    print(2)
-    try:
-        token = await oauth.steam.authorize_access_token(request)
-        user = await oauth.steam.parse_id_token(request, token)
-        # Здесь можно сохранить информацию о пользователе в базе данных
-        return {'message': f'Logged in as {user["name"]}'}
-    except OAuthError as e:
-        return {'error': str(e)}
+    return response.json()['response']['players'][0]
