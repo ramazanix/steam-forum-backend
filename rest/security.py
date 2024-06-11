d78cda77156ce47b01077c36296a0fbbbc750977
ramazanix <ramazanui2002@gmail.com>
2024-06-11 18:15:30 +0300

    Added func for creating jwt tokens

---
+++ rest/security.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime, timezone, timedelta
+import jwt
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(seconds=int(os.getenv('ACCESS_EXPIRES')))
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, os.getenv('SECRET_KEY'), algorithm=os.getenv('ALGORITHM'))
+    return encoded_jwt
