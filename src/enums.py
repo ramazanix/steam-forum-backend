595c683e92d094510e09ae427a56293ee2b0b6e4
ramazanix <ramazanui2002@gmail.com>
2024-06-30 17:02:55 +0300

    Added enums for steam currencies and app ids

---
+++ src/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class SteamCurrencies(Enum):
+    USD = "1"
+    GBP = "2"
+    EUR = "3"
+    RUB = "5"
+
+
+class SteamAppIds(Enum):
+    DOTA2 = "570"
+    CS2 = "730"
